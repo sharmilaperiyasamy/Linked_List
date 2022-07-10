dcd794cc6e4f78dcf70a2c535f90c6cb00013920
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-10 14:37:36 +0530

    [Sharmila] uc1 Simple Linked List

---
+++ Linked_List_DataStructures/Linked_List.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List_DataStructures
+{
+    internal class Linked_List
+    {
+        Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(data + " is inserted into the linked list.");
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty.");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(temp.data + "->");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ Linked_List_DataStructures/Program.cs
@@ -0,0 +1,5 @@
+﻿Linked_List_DataStructures.Linked_List list = new Linked_List_DataStructures.Linked_List();
+list.Add(56);
+list.Add(30);
+list.Add(70);
+list.Display();
