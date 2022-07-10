01dd0a4f4d19d10cf6782426c056044a682566b0
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-10 15:55:09 +0530

    [Sharmila] Append the data to the list

--- Linked_List_DataStructures/Linked_List.cs
+++ Linked_List_DataStructures/Linked_List.cs
@@ -43,5 +43,22 @@ namespace Linked_List_DataStructures
                 }
             }
         }
+        public void Append(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(data + " is added to the linked list.");
+
+        }
     }
 }
--- Linked_List_DataStructures/Program.cs
+++ Linked_List_DataStructures/Program.cs
@@ -1,12 +1,28 @@
 ﻿Linked_List_DataStructures.Linked_List list = new Linked_List_DataStructures.Linked_List();
 
-Console.WriteLine("Enter how many values need to the list : ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-for (int i = 0; i < n; i++)
+Console.WriteLine("\nLinkedList Operations:\n0.Exit\n1.Adding data to the list\n2.Display the data in the list\n3.Append the data\nEnter your choice:");
+int option = Convert.ToInt32(Console.ReadLine());
+while (option != 0)
 {
-    Console.WriteLine("\nEnter the data add to the list : ");
-    int value = Convert.ToInt32(Console.ReadLine());
-    list.Add(value);
-    list.Display();
+    switch (option)
+    {
+        case 1:
+            Console.WriteLine("Enter the data to add to the list : ");
+            int data = Convert.ToInt32(Console.ReadLine());
+            list.Add(data);
+            break;
+        case 2:
+            list.Display();
+            break;
+        case 3:
+            Console.WriteLine("Enter data to append.");
+            int value = Convert.ToInt32(Console.ReadLine());
+            list.Append(value);
+            break;
+        default:
+            Console.WriteLine("Invalid option.");
+            break;
+    }
+    Console.WriteLine("\nLinkedList Operations:\n0.Exit\n1.Adding data to the list\n2.Display the data in the list\n3.Append the data\nEnter your choice:");
+    option = Convert.ToInt32(Console.ReadLine());
 }
